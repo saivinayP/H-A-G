4ebe1bbcd0154a27ba8e1627e4801b55bbf371ce
saivinayP <paladugusaivinay@gmail.com>
2026-01-03 14:21:05 +0530

    add csv parsing engine

---
+++ hag-core/src/main/java/com/hag/core/engine/parser/CsvHeaders.java
@@ -0,0 +1,10 @@
+package com.hag.core.engine.parser;
+
+import java.util.List;
+
+public final class CsvHeaders {
+    private CsvHeaders() {}
+
+    public static final List<String> REQUIRED_HEADERS =
+            List.of("Action", "Recipient", "Source", "Key");
+}
